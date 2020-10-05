d3ace99badd2b1708a8672c11fc78b8e6d014012
justincruz97 <61398634+justincruz97@users.noreply.github.com>
2020-10-05 13:51:50 -0700

    Updated file for readability
    created a main function to test firstLast function.

--- name_finder.py
+++ name_finder.py
@@ -1,4 +1,6 @@
 # Name: Justin Cruz
+# This file will locate a name inside a repository
+# and will determine if the file contains a name
 
 import os.path
 import sys
@@ -11,7 +13,6 @@ def firstLast(file_name):
             if( "name:" in low_line ):
                 name_exists = True    
                 split_line = low_line.split()
-                #print(split_line)
                 idx = split_line.index("name:")
                 first = split_line[idx + 1].upper()
                 last = split_line[idx + 2].upper()
@@ -20,32 +21,25 @@ def firstLast(file_name):
                 break
         if( name_exists == False ):
             print("%s contains no name." %file_name)
-
-# IF INPUT TEXT FILE EXISTS
-if ( len(sys.argv) > 1 ):
-    input_file = sys.argv[1]
-    if( os.path.isfile(input_file) ):
-        firstLast(input_file)
-    # INPUT FILE DOESN'T EXIST
+               
+def main():
+    # if input text file exists
+    if ( len(sys.argv) > 1 ):
+        input_file = sys.argv[1]
+        if( os.path.isfile(input_file) ):
+            firstLast(input_file)
+            
+        # if input file doesn't exist
+        else:
+            print("%s doesn't exist" %input_file)
+            
+    # using a default me.txt
     else:
-        print("%s doesn't exist" %input_file)
-# USING DEFAULT ME.TXT
-else:
-    if( os.path.isfile("me.txt") ):
-        firstLast("me.txt")
-    else:
-        print("\"me.txt\" doesn't exist")
+        if( os.path.isfile("me.txt") ):
+            firstLast("me.txt")
+        else:
+            print("\"me.txt\" doesn't exist")
 
+if __name__ == "__main__":
+    main()
 
-
-first = 3
-second = 20
-
-def swirl(first,second=3):
-    first,second = second,first
-    return first
-
-result = swirl(swirl(first),second)
-result = swirl(second,result)
-
-print(result)
